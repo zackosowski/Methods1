f0d76b2070f76c1e923365578ea419b1ccab01db
zackosowski <ZACHARYO@MYSTMA.ORG>
2020-11-09 14:06:51 +0000

    Created Assignment

--- JavaTemplate/src/app/App.java
+++ JavaTemplate/src/app/App.java
@@ -1,7 +1,27 @@
+/*
+METHODS 1
+
+Create a program containing three methods.
+One method should simply print out "Hello World"
+One method should print out a given favorite color.
+One method should add two given numbers together.
+
+HINT
+
+Remeber the syntax for methods
+
+public static void name(type name){
+
+}
+
+*/
+
+
+
 package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+      
     }
 }
